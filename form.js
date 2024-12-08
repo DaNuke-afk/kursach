2ca5c7d36551214477f2097779fd742630bc8fd4
DaNuke-afk <ds25032005@gmail.com>
2024-12-09 00:54:16 +0300

    Update form.js

--- form.js
+++ form.js
@@ -1,34 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
     const toggle = document.getElementById("toggle");
     const formTitle = document.getElementById("form-title");
-    const registrationFields = document.getElementById("registration-fields");
-    const loginFields = document.getElementById("login-fields");
+    const regForm = document.getElementById("reg-form");
+    const logForm = document.getElementById("log-form");
     const registerLabel = document.getElementById("register-label");
     const loginLabel = document.getElementById("login-label");
-    const form = document.getElementById("form");
 
     // Переключение между "Регистрация" и "Войти"
     toggle.addEventListener("change", () => {
         if (toggle.checked) {
             // Режим "Войти"
             formTitle.textContent = "Войти";
-            registrationFields.style.display = "none";
-            loginFields.style.display = "block";
+            regForm.style.display = "none";
+            logForm.style.display = "block";
             loginLabel.classList.add("active");
             registerLabel.classList.remove("active");
         } else {
             // Режим "Регистрация"
             formTitle.textContent = "Регистрация";
-            registrationFields.style.display = "block";
-            loginFields.style.display = "none";
+            regForm.style.display = "block";
+            logForm.style.display = "none";
             registerLabel.classList.add("active");
             loginLabel.classList.remove("active");
         }
     });
 
     // Переход на страницу ind.html после отправки формы
-    form.addEventListener("submit", (event) => {
+    regForm.addEventListener("submit", (event) => {
         event.preventDefault(); // Предотвращаем стандартное поведение формы
-        window.location.href = "main.html"; // Переход на страницу main.html
+        window.location.href = "main.html"; // Переход на страницу ind.html
+    });
+    logForm.addEventListener("submit", (event) => {
+        event.preventDefault(); // Предотвращаем стандартное поведение формы
+        window.location.href = "main.html"; // Переход на страницу ind.html
     });
 });
